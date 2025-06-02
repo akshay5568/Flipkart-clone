33f4fe747d2d659c84be4d85f4d11d3642b8af18
akshay5568 <adityajangid124@gmail.com>
2025-06-03 00:46:27 +0530

    Navbar updated some hover triggrid-tooltip / popover.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,69 @@
 import {NavLink} from "react-router-dom";
+import { IoMdLogIn } from "react-icons/io";
+import { BsCart3 } from "react-icons/bs";
+import { CiShop } from "react-icons/ci";
+import { CiMenuKebab } from "react-icons/ci";
+import { CgProfile } from "react-icons/cg";
+import { FaBoxOpen } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
+import { FaAngleDown } from "react-icons/fa";
 
 function Navbar() {
     return (
-        <div className=" p-3 ">
+        <div className="p-3 ">
             <nav className=" flex pt-2 justify-between item-center" >
                 <NavLink to='/'><img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/fkheaderlogo_exploreplus-44005d.svg" alt="" /></NavLink>
-                <input className="w-[40rem] p-2 rounded-md bg-[#f0f5ff]" type="text" placeholder="Search For Products, Brands and More"/>
-              <div className="flex gap-30 item-center">
-                <NavLink to='/login'>Login</NavLink>
-                <NavLink to='/cart'>Cart</NavLink>
-                <NavLink to='/sell-online'>Become a Seller</NavLink>
-                <NavLink to='/'>:</NavLink>
-              </div>    
+                <input className="w-[40rem] p-2 rounded-md bg-[#f0f5ff]" type="text" placeholder="🔍  Search For Products, Brands and More"/>
+
+          <div className="flex gap-20 items-center">
+
+                <div className="flex relative group items-center gap-2 w-[7rem] h-[3rem] rounded-md ml-3 justify-center bg-teal-100 hover:bg-[#2c64e3] hover:text-white" >
+                    <IoMdLogIn />  
+                    <NavLink to='/login'>Login</NavLink>
+                    <FaAngleDown />
+
+
+                       <div className="w-[15rem] h-[10rem] absolute text-black top-13 left-0 opacity-0 group-hover:opacity-100 bg-[#ffffff] rounded-md p-3 transition-opacity duration-300">
+                        <div className="flex justify-between">
+                            <h3>New Custumer?</h3>
+                            <NavLink to='/signup'>Signup</NavLink>
+                        </div>
+
+                        <div className="flex mt-3 items-center gap-2">
+                            <CgProfile />
+                            <NavLink to='/myproflie'>My Profile</NavLink>
+                        </div>
+
+                        <div className="flex mt-3 items-center gap-2">
+                          <FaBoxOpen />
+                            <NavLink to='/orders'>Orders</NavLink>
+                        </div>
+
+                        <div className="flex mt-3 items-center gap-2">
+                          <FaRegHeart />
+                            <NavLink to='/wishlist'>Wishlist</NavLink>
+                        </div>
+
+                </div>
+                </div> 
+
+             
+
+                <div className="flex items-center gap-2">
+                    <BsCart3 />
+                    <NavLink to='/cart'>Cart</NavLink>
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <CiShop />
+                    <NavLink to='/sell-online'>Become a Seller</NavLink>
+                </div>
+                    
+                <div className="flex items-center pr-9">
+                  <NavLink to='/'><CiMenuKebab /></NavLink>
+                </div>    
+
+         </div>    
             </nav>
         </div>
     )
