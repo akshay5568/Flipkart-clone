938477eff2c904c88d861a9fee8461fee1a8355b
akshay5568 <adityajangid124@gmail.com>
2025-06-02 23:29:19 +0530

    Complete landing page and added search bar in nav.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ function Navbar() {
         <div className=" p-3 ">
             <nav className=" flex pt-2 justify-between item-center" >
                 <NavLink to='/'><img src="https://static-assets-web.flixcart.com/batman-returns/batman-returns/p/images/fkheaderlogo_exploreplus-44005d.svg" alt="" /></NavLink>
+                <input className="w-[40rem] p-2 rounded-md bg-[#f0f5ff]" type="text" placeholder="Search For Products, Brands and More"/>
               <div className="flex gap-30 item-center">
                 <NavLink to='/login'>Login</NavLink>
                 <NavLink to='/cart'>Cart</NavLink>
